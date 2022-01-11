fc365461359f271dd2e25b9e36a5ea648c2856dc
shimsangwon <sangwon.shim@powertask.biz>
2022-01-11 11:25:45 +0900

    안정버전

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const semver = require('semver');
 
 const VERSION = 'version';
 const REFS = 'refs'
 
+/**
+ * 1. 수동으로 입력한 값이 있다면, 우선적용
+ * 2. 만약 없다면 현재값 적용
+ * 3. 그런데, semver 형식이 아니라면 에러
+ */
+
 try {
   const manuallyVersion = core.getInput(VERSION);
-  const refs = core.getInput(REFS);
+  const ref = github.context.ref
 
-  if (refs !== '') {
-    const versions = refs.split('/');
-    const version = versions[versions.length - 1];
-
-    console.log(github.context.ref);
-    core.setOutput('version', version);
-  } else if (manuallyVersion !== '') {
-    console.log(manuallyVersion);
-    core.setOutput('version', manuallyVersion);
+  let version = null;
+  if (manuallyVersion !== '') {
+    version = semver.coerce(manuallyVersion, {});
   } else {
-    core.setOutput('version', '1.0.0');
+    const refs = ref.split('/');
+    version = refs[refs.length - 1];
+  }
+
+  if (semver.valid(version, {})) {
+    core.setOutput('version', version);
+  } else {
+    core.setFailed(`입력한 버전(${manuallyVersion !== '' ? manuallyVersion : ref}, ${version}) 이 SEMVER 형식에 맞지 않습니다.`);
   }
 } catch (e) {
-  core.setOutput('version', '1.0.0');
+  core.setFailed(e.message);
 }
